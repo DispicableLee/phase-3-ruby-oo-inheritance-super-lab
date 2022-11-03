0433b5ac9630bcd336fc0c4995586cb92883459b
DispicableLee <lee.robert053@gmail.com>
2022-11-03 10:13:18 -0400

    done

---
+++ lib/student.rb
@@ -0,0 +1,15 @@
+class Student
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+    
+    def hello
+        "Hey there! I'm excited to learn stuff!"
+    end
+
+    def raise_hand
+        "pick me!"
+    end
+    
+end
